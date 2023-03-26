5520dfe2952be24bed442b08184f086428a722c7
100QN <83006473+100QNKolev@users.noreply.github.com>
2023-03-26 14:12:49 +0300

    Added Receipt class

---
+++ Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    public class Controller
+    {
+        public string ProcessProductCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string ProcessServiceCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string ProcessCheckoutCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string ProcessInfoCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string ProcessEndCommand()
+        {
+            //TODO: Add some logic here …
+        }
+    }
+
+}
---
+++ Receipt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    public class Receipt
+    {
+        private string customerName;
+        private List<Product> products;
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                if (value.Length < 2 || value.Length > 40)
+                {
+                    throw new ArgumentException("Customer Name should be between 2 and 40 characters!");
+                }
+                customerName = value;
+            }
+        }
+
+        public Receipt(string customerName)
+        {
+            this.CustomerName = customerName;
+        }
+
+        public void AddProduct(Product product) 
+        {
+          //TODO: create AddProduct function
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+            //TODO: create ToString function
+        }
+    }
+}
