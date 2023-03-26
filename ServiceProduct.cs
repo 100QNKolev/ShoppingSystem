35fdb56bce67207a4285bfa243a95698d36e8ff3
100QN <83006473+100QNKolev@users.noreply.github.com>
2023-03-26 14:06:16 +0300

    Added ServiceProduct class

---
+++ ServiceProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    public class ServiceProduct : Product
+    {
+        private double time;
+
+        public double Time
+        {
+            get { return time; }
+            set {
+                if (time < 0)
+                {
+                    throw new ArgumentException("Time should be greater than 0!");
+                }
+                time = value;
+            }
+        }
+
+
+        public ServiceProduct(string name, double price, double time) : base(name, price)
+        {
+            this.Time = time;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+            //TODO: create ToString function
+        }
+    }
+}
